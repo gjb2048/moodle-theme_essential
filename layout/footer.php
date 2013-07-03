6a6cf5fd00680114cddd3ba8a7ba3b9bdde476e8
Julian Ridden <julian@moodleman.net>
2013-07-03 23:01:44 +1000

    Making block regions dragable

--- layout/footer.php
+++ layout/footer.php
@@ -26,33 +26,47 @@
 
 $hascopyright = (empty($PAGE->theme->settings->copyright)) ? false : $PAGE->theme->settings->copyright;
 ?>
-<div class="row-fluid">
-    <!-- Widget 1 -->
-    <div class="span4">
-        <?php if ($hasfooterleft) {
-            echo $OUTPUT->blocks_for_region('footer-left');
-        } ?>
-    </div>
+	<div class="row-fluid">
+    	<!-- Widget 1 -->
+    	<div class="span4">
+    		<div id="footer-left" class="block-region">
+    			<div class="region-content">
+       			<?php if ($hasfooterleft) {
+            		echo $OUTPUT->blocks_for_region('footer-left');
+        		} ?>
+        		</div>
+        	</div>
+    	</div>
 
-    <!-- widget 2 -->
-    <div class="span4">
-        <?php if ($hasfootermiddle) {
-            echo $OUTPUT->blocks_for_region('footer-middle');
-        } ?>
-    </div>
+    	<!-- widget 2 -->
+    	<div class="span4">
+    		<div id="footer-middle" class="block-region">
+    			<div class="region-content">
+       			<?php if ($hasfootermiddle) {
+            		echo $OUTPUT->blocks_for_region('footer-middle');
+        		} ?>
+        		</div>
+        	</div>
+    	</div>
 
-    <!-- Widget 3 -->
-    <div class="span4">
-        <?php if ($hasfooterright) {
-            echo $OUTPUT->blocks_for_region('footer-right');
-        } ?>
-    </div>
-</div>
+    	<!-- Widget 3 -->
+    	<div class="span4">
+    		<div id="footer-right" class="block-region">
+    			<div class="region-content">
+       			<?php if ($hasfooterright) {
+            		echo $OUTPUT->blocks_for_region('footer-right');
+        		} ?>
+        		</div>
+        	</div>
+    	</div>
+	</div>
 
-<div class="row-fluid">
-    <hr>
-    <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')); ?></p>
-    <?php if ($hascopyright) {
-        echo '    <p class="copy">&copy; '.date("Y").' '.$hascopyright.'</p>';
-    } ?>
-</div>
+	<div class="row-fluid">
+    	<hr>
+    	<p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')); ?></p>
+    	<?php if ($hascopyright) {
+        	echo '    <p class="copy">&copy; '.date("Y").' '.$hascopyright.'</p>';
+    	} ?>
+	</div>
+	
+
