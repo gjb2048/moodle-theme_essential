6f6fe553079d57786e6c0ba751d57121f8803213
Julian Ridden <julian@moodleman.net>
2013-07-18 12:09:06 +1000

    Fix issues with standard layout

--- layout/standard.php
+++ layout/standard.php
@@ -1,23 +1,62 @@
+
+
+
+
+
+
 <div id="page" class="container-fluid">
 
-    <div id="page-content" class="row-fluid">
-        <div id="<?php echo $regionbsid ?>" class="span12">
-            <div class="row-fluid">
-                <section id="region-main" class="span8 pull-right">
-                	<?php if ($hasnavbar) { ?>
+<div id="page-content" class="row-fluid">
+
+<?php if ($layout === 'pre-and-post') { ?>
+    <div id="region-bs-main-and-pre" class="span9">
+    <div class="row-fluid">
+    <section id="region-bs-main" class="span8 pull-right">
+<?php } else if ($layout === 'side-post-only') { ?>
+    <section id="region-bs-main" class="span9">
+<?php } else if ($layout === 'side-pre-only') { ?>
+    <section id="region-bs-main" class="span9 pull-right">
+<?php } else if ($layout === 'content-only') { ?>
+    <section id="region-bs-main" class="span12">
+<?php } ?>
+	<?php if ($hasnavbar) { ?>
             			<nav class="breadcrumb-button"><?php echo $PAGE->button; ?></nav>
             			<?php echo $OUTPUT->navbar(); ?>
             		<?php } ?>
 
-                    <?php echo $OUTPUT->course_content_header(); ?>
-                    <h2 class="pagetitle"><span><?php echo $PAGE->title ?></span></h2>
-                    <?php echo $OUTPUT->main_content();
-                    echo $OUTPUT->course_content_footer();
-                    ?>
-                </section>
-                <?php echo $OUTPUT->blocks('side-pre', 'span4 desktop-first-column'); ?>
-            </div>
-        </div>
-        <?php echo $OUTPUT->blocks('side-post', 'span3'); ?>
-    </div>
+    <?php echo $coursecontentheader; ?>
+    <h2 class="pagetitle"><span><?php echo $PAGE->title ?></span></h2>
+    <?php echo $OUTPUT->main_content() ?>
+    <?php echo $coursecontentfooter; ?>
+    </section>
+
+
+<?php if ($layout !== 'content-only') {
+          if ($layout === 'pre-and-post') { ?>
+            <aside id="region-pre" class="span4 block-region desktop-first-column region-content">
+    <?php } else if ($layout === 'side-pre-only') { ?>
+            <aside id="region-pre" class="span3 block-region desktop-first-column region-content">
+    <?php } ?>
+          <?php
+                if (!right_to_left()) {
+                    echo $OUTPUT->blocks_for_region('side-pre');
+                } else if ($hassidepost) {
+                    echo $OUTPUT->blocks_for_region('side-post');
+                }
+            ?>
+            </aside>
+    <?php if ($layout === 'pre-and-post') {
+          ?></div></div><?php // Close row-fluid and span9.
+   }
+
+    if ($layout === 'side-post-only' OR $layout === 'pre-and-post') { ?>
+        <aside id="region-post" class="span3 block-region region-content">
+        <?php if (!right_to_left()) {
+                  echo $OUTPUT->blocks_for_region('side-post');
+              } else {
+                  echo $OUTPUT->blocks_for_region('side-pre');
+              } ?>
+        </aside>
+    <?php } ?>
+<?php } ?>
 </div>
