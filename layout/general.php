ff3a3b8f387b9f5953e2a67e3a2a5a5d386b4995
Julian Ridden <julian@moodleman.net>
2013-07-18 10:11:44 +1000

    Have general layout pick up new layout theme setting

--- layout/general.php
+++ layout/general.php
@@ -42,6 +42,7 @@ $showsidepost = ($hassidepost && !$PAGE->blocks->region_completely_docked('side-
 $showfooterleft = ($hasfooterleft && !$PAGE->blocks->region_completely_docked('footer-left', $OUTPUT));
 $showfootermiddle = ($hasfootermiddle && !$PAGE->blocks->region_completely_docked('footer-middle', $OUTPUT));
 $showfooterright = ($hasfooterright && !$PAGE->blocks->region_completely_docked('footer-right', $OUTPUT));
+$hasboringlayout = (empty($PAGE->theme->settings->layout)) ? false : $PAGE->theme->settings->layout;
 
 // If there can be a sidepost region on this page and we are editing, always
 // show it so blocks can be dragged into it.
@@ -133,61 +134,11 @@ if ($hasheader) {
     </nav>
 </header>
 
-<div id="page" class="container-fluid">
-        
-    <div id="page-content" class="row-fluid">
-
-        <?php if ($layout === 'pre-and-post') { ?>
-        <section id="region-main" class="span6 desktop-first-column">
-        <?php } else if ($layout === 'side-post-only') { ?>
-        <section id="region-main" class="span8 desktop-first-column">
-        <?php } else if ($layout === 'side-pre-only') { ?>
-        <section id="region-main" class="span9 desktop-first-column">
-        <?php } else if ($layout === 'content-only') { ?>
-        <section id="region-main" class="span12">
-        <?php } ?>
-            <?php if ($hasnavbar) { ?>
-            <nav class="breadcrumb-button"><?php echo $PAGE->button; ?></nav>
-            <?php echo $OUTPUT->navbar(); ?>
-            <?php } ?>
-            <?php echo $coursecontentheader; ?>
-            <h2 class="pagetitle"><span><?php echo $PAGE->title ?></span></h2>  
-            <?php echo $OUTPUT->main_content() ?>
-            <?php echo $coursecontentfooter; ?>
-        </section>
-        <?php if ($layout === 'pre-and-post') { ?>
-        <div class="span6">
-        <?php } else if ($layout === 'side-post-only') { ?>
-        <div class="span3">
-        <?php } else if ($layout === 'side-pre-only') { ?>
-        <div class="span3">
-        <?php } else if ($layout === 'content-only') { ?>
-        <div class="span0">
-        <?php } ?>
-            <?php if ($layout === 'pre-and-post') { ?>
-            <div class="span6">
-            <?php } else { ?>
-            <div class="span12" id="move">
-            <?php } ?>
-                <div id="region-post" class="block-region">
-                    <div class="region-content">
-                    <?php echo $OUTPUT->blocks_for_region('side-post'); ?>
-                    </div>
-                </div>
-            </div>
-            <?php if ($layout === 'pre-and-post') { ?>
-            <div class="span6">
-            <?php } else { ?>
-            <div class="span12">
-            <?php } ?>
-                <div id="region-pre" class="block-region">
-                    <div class="region-content">
-                    <?php echo $OUTPUT->blocks_for_region('side-pre'); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>    
+<?php if ($hasboringlayout) { ?>
+		<?php require('standard.php'); ?>
+	<?php } else { ?>
+		<?php require('essential.php'); ?>
+	<?php } ?>  
 
 <footer id="page-footer" class="container-fluid">
             <?php require('footer.php'); ?>
