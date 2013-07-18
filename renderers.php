1631c472501120c4c44aceafcf5ed8cb2ce2e784
Julian Ridden <julian@moodleman.net>
2013-07-18 10:27:36 +1000

    Updated renderer to generate fontawesome icons for Admin menu

---
+++ renderers.php
@@ -0,0 +1,153 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Renderers to align Moodle's HTML with that expected by Bootstrap
+ *
+ * @package    theme_essential
+ * @copyright  2012
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+ class theme_essential_core_renderer extends core_renderer {
+ 	
+ 	protected function render_custom_menu(custom_menu $menu) {
+        // If the menu has no children return an empty string.
+        if (!$menu->has_children()) {
+            return '';
+        }
+        $addlangmenu = true;
+        $langs = get_string_manager()->get_list_of_translations();
+            if ($this->page->course != SITEID and !empty($this->page->course->lang)) {
+            // Do not show lang menu if language forced.
+            $addlangmenu = false;
+        }
+        if (count($langs) < 2) {
+            $addlangmenu = false;
+        }
+
+        if ($addlangmenu) {
+            $language = $menu->add(get_string('language'), new moodle_url('#'), get_string('language'), 10000);
+            foreach ($langs as $langtype => $langname) {
+                $language->add($langname,
+                new moodle_url($this->page->url, array('lang' => $langtype)), $langname);
+            }
+        }
+
+        $content = '<ul class="nav">';
+        foreach ($menu->get_children() as $item) {
+            $content .= $this->render_custom_menu_item($item, 1);
+        }
+
+        return $content.'</ul>';
+    }
+
+    /*
+     * This code renders the custom menu items for the
+     * bootstrap dropdown menu.
+     */
+    protected function render_custom_menu_item(custom_menu_item $menunode, $level = 0 ) {
+        static $submenucount = 0;
+
+        if ($menunode->has_children()) {
+
+            if ($level == 1) {
+                $dropdowntype = 'dropdown';
+            } else {
+                $dropdowntype = 'dropdown-submenu';
+            }
+
+            $content = html_writer::start_tag('li', array('class'=>$dropdowntype));
+            // If the child has menus render it as a sub menu.
+            $submenucount++;
+            if ($menunode->get_url() !== null) {
+                $url = $menunode->get_url();
+            } else {
+                $url = '#cm_submenu_'.$submenucount;
+            }
+            $content .= html_writer::start_tag('a', array('href'=>$url, 'class'=>'dropdown-toggle', 'data-toggle'=>'dropdown'));
+            $content .= $menunode->get_title();
+            if ($level == 1) {
+                $content .= '<b class="caret"></b>';
+            }
+            $content .= '</a>';
+            $content .= '<ul class="dropdown-menu">';
+            foreach ($menunode->get_children() as $menunode) {
+                $content .= $this->render_custom_menu_item($menunode, 0);
+            }
+            $content .= '</ul>';
+        } else {
+            $content = '<li>';
+            // The node doesn't have children so produce a final menuitem.
+            if ($menunode->get_url() !== null) {
+                $url = $menunode->get_url();
+            } else {
+                $url = '#';
+            }
+            $content .= html_writer::link($url, $menunode->get_text(), array('title'=>$menunode->get_title()));
+        }
+        return $content;
+    }
+ 	
+ 	protected function render_pix_icon(pix_icon $icon) {
+        if (self::replace_moodle_icon($icon->pix) !== false && $icon->attributes['alt'] === '' && $icon->attributes['title'] === '') {
+            return self::replace_moodle_icon($icon->pix);
+        } else {
+            return parent::render_pix_icon($icon);
+        }
+    }
+    private static function replace_moodle_icon($name) {
+        $icons = array(
+            'add' => 'plus',
+            'book' => 'book',
+            'chapter' => 'file',
+            'docs' => 'question-sign',
+            'generate' => 'gift',
+            'i/backup' => 'upload-alt',
+            'i/checkpermissions' => 'user',
+            'i/edit' => 'pencil',
+            'i/filter' => 'filter',
+            'i/grades' => 'table',
+            'i/group' => 'group',
+            'i/hide' => 'eye-open',
+            'i/import' => 'download-alt',
+            'i/move_2d' => 'move',
+            'i/navigationitem' => 'sign-blank',
+            'i/publish' => 'globe',
+            'i/reload' => 'refresh',
+            'i/report' => 'list-alt',
+            'i/restore' => 'download-alt',
+            'i/return' => 'repeat',
+            'i/roles' => 'user',
+            'i/settings' => 'cog',
+            'i/show' => 'eye-close',
+            'i/user' => 'user',
+            'i/users' => 'user',
+        );
+        if (isset($icons[$name])) {
+            return "<i class=\"icon-$icons[$name]\" id=\"icon\"></i>";
+        } else {
+            return false;
+        }
+    }
+
+    public function larrow() {
+        return '<i class="icon-chevron-left"></i>';
+    }
+    public function rarrow() {
+        return '<i class="icon-chevron-right"></i>';
+    }
+}
