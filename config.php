61816e713de65a52136a8ac271d607cc2f591ac1
Julian Ridden <julian@moodleman.net>
2013-07-18 22:31:50 +1000

    Fixes to allow drag and drop in edit mode

--- config.php
+++ config.php
@@ -71,7 +71,7 @@ $THEME->layouts = array(
     'course' => array(
         'file' => 'course.php',
         'regions' => array('side-pre', 'side-post', 'footer-left', 'footer-middle', 'footer-right'),
-        'defaultregion' => 'side-post',
+        'defaultregion' => 'side-pre',
          'options' => array('nonavbar'=>false),
     ),
     // Page content and modules.
@@ -109,8 +109,14 @@ $THEME->layouts = array(
     ),
     // Server administration scripts.
     'admin' => array(
-        'file' => 'general.php',
+        'file' => 'report.php',
         'regions' => array('side-pre', 'side-post', 'footer-left', 'footer-middle', 'footer-right'),
+        'defaultregion' => 'side-pre',
+    ),
+    // Report Pages.
+    'report' => array(
+        'file' => 'report.php',
+        'regions' => array('side-pre', 'footer-left', 'footer-middle', 'footer-right'),
         'defaultregion' => 'side-pre',
     ),
 
--- layout/standard.php
+++ layout/standard.php
@@ -1,11 +1,4 @@
-
-
-
-
-
-
 <div id="page" class="container-fluid">
-
 <div id="page-content" class="row-fluid">
 
 <?php if ($layout === 'pre-and-post') { ?>
@@ -18,12 +11,12 @@
     <section id="region-bs-main" class="span9 pull-right">
 <?php } else if ($layout === 'content-only') { ?>
     <section id="region-bs-main" class="span12">
-<?php } ?>
-	<?php if ($hasnavbar) { ?>
-            			<nav class="breadcrumb-button"><?php echo $PAGE->button; ?></nav>
-            			<?php echo $OUTPUT->navbar(); ?>
-            		<?php } ?>
+<?php }
 
+	if ($hasnavbar) { ?>
+		<nav class="breadcrumb-button"><?php echo $PAGE->button; ?></nav>
+		<?php echo $OUTPUT->navbar();
+	} ?>
     <?php echo $coursecontentheader; ?>
     <h2 class="pagetitle"><span><?php echo $PAGE->title ?></span></h2>
     <?php echo $OUTPUT->main_content() ?>
@@ -38,11 +31,20 @@
             <aside id="region-pre" class="span3 block-region desktop-first-column region-content">
     <?php } ?>
           <?php
-                if (!right_to_left()) {
-                    echo $OUTPUT->blocks_for_region('side-pre');
-                } else if ($hassidepost) {
-                    echo $OUTPUT->blocks_for_region('side-post');
-                }
+                if (!right_to_left()) { ?>
+                  <div id="region-pre" class="block-region">
+                  <div class="region-content">
+                  <?php echo $OUTPUT->blocks_for_region('side-pre'); ?>
+                  </div>
+                  </div>
+                <?php } 
+                else if ($hassidepost) { ?>
+                  <div id="region-post" class="block-region">
+                  <div class="region-content">
+                  <?php echo $OUTPUT->blocks_for_region('side-post');?>
+                  </div>
+                  </div>
+                <?php }
             ?>
             </aside>
     <?php if ($layout === 'pre-and-post') {
@@ -51,12 +53,24 @@
 
     if ($layout === 'side-post-only' OR $layout === 'pre-and-post') { ?>
         <aside id="region-post" class="span3 block-region region-content">
-        <?php if (!right_to_left()) {
-                  echo $OUTPUT->blocks_for_region('side-post');
-              } else {
-                  echo $OUTPUT->blocks_for_region('side-pre');
-              } ?>
+        <?php 
+        	if (!right_to_left()) { ?>
+                  <div id="region-post" class="block-region">
+                  <div class="region-content">
+                  <?php echo $OUTPUT->blocks_for_region('side-post'); ?>
+                  </div>
+                  </div>
+              <?php } 
+              else { ?>
+                  <div id="region-pre" class="block-region">
+                  <div class="region-content">
+                  <?php echo $OUTPUT->blocks_for_region('side-pre'); ?>
+                  </div>
+                  </div>
+              <?php } 
+          ?>
         </aside>
     <?php } ?>
 <?php } ?>
+</div>
 </div>
