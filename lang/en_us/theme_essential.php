fee21375c32a37cdf9eadfcd3097aada4d99ed8d
Gareth J Barnard <gjbarnard@gmail.com>
2013-08-10 00:05:11 +0100

    Language improvements

---
+++ lang/en_us/theme_essential.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This is built using the Clean template to allow for new theme's using
+ * Moodle's new Bootstrap theme engine
+ *
+ *
+ * @package   theme_essential
+ * @copyright 2013 Julian Ridden
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/* General */
+$string['editiconsdesc'] = 'This utilises the Font Awesome to display colored and neater formatted edit icons in course pages and blocks. Please note that currsently this is NOT compatible with Autohide.';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+$string['copyrightdesc'] = 'The name of your organization.';
+
+/* Colors */
+$string['colorheading'] = 'Color Settings';
+$string['colorheadingsub'] = 'Set the colors used in your theme';
+$string['colordesc'] = 'Here you can find various settings to change many of the colors found in this theme.';
+
+$string['themecolor'] = 'Theme Color';
+$string['themecolordesc'] = 'What color should your theme be. This will change mulitple components to produce the color you wish across the moodle site';
+$string['themehovercolor'] = 'Theme Hover Color';
+$string['themehovercolordesc'] = 'What color should your theme hovers be. This is used for links, menus, etc';
+$string['footercolor'] = 'Footer Background Color';
+$string['footercolordesc'] = 'Set what color the background of the Footer box should be.';
+$string['footersepcolor'] = 'Footer Seperator Color';
+$string['footersepcolordesc'] = 'Seperators are lines used to seperate content. Set their color here.';
+$string['footertextcolor'] = 'Footer Text Color';
+$string['footertextcolordesc'] = 'Set the color you want your text to be in the footer.';
+$string['footerurlcolor'] = 'Footer Link Color';
+$string['footerurlcolordesc'] = 'Set the color for your linked text in the footer.';
+$string['footerhovercolor'] = 'Footer Link Hover Color';
+$string['footerhovercolordesc'] = 'Set the color for your linked text when hovered over in the footer.';
+$string['footerheadingcolor'] = 'Footer Heading Color';
+$string['footerheadingcolordesc'] = 'Set the color for block headings in the footer.';
