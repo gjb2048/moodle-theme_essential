f81180203875b5d0a9421a55810b5ebbc2dfa2ae
Gareth J Barnard <gjbarnard@gmail.com>
2013-08-11 22:53:08 +0100

    English Pirate file.

---
+++ lang/en_ar/theme_essential.php
@@ -0,0 +1,269 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: yee can redistribute it and/or modify
+// it under thee terms of thee GNU General Public License as published by
+// thee Free Software Foundation, eitheer version 3 of thee License, or
+// (at yee option) any later version.
+//
+// Moodle is distributed in thee hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even thee implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See thee
+// GNU General Public License for more details.
+//
+// yee should have received a copy of thee GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This is built using thee Clean template to allow for new theme's using
+ * Moodle's new Bootstrap theme engine
+ *
+ *
+ * @package   theme_essential
+ * @copyright 2013 Julian Ridden
+ * @copyright 2013 Gareth J Barnard - Pirate translation.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/* Core */
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well">
+<h2>Essential</h2>
+<p><img class=img-polaroid src="essential/pix/screenshot.jpg" /></p>
+</div>
+<div class="well">
+<h3>About Essential mee hearties</h3>
+<p>Essential is thee modified Moodle bootstrap theme which inherits styles and renderers from its parent theme, arrg.</p>
+<h3>Parents</h3>
+<p>Thy theme is based upon thee Bootstrap theme, which thy was created for Moodle 2.5, with thee help of thy ship mates:<br>
+Stuart Lamour, Mark Aberdour, Paul Hibbitts and Mary Evans.</p>
+<h3>Theme Credits</h3>
+<p>Author: Captain Julian Ridden<br>
+Contact: julian@moodleman.net<br>
+Website: <a href="http://www.moodleman.net">www.moodleman.net</a>
+</p>
+</div></div>';
+
+/* General */
+$string['geneicsettings'] = 'Thy General Settings';
+$string['autohide'] = 'Include thee Autohide Functionality';
+$string['autohidedesc'] = 'Thee Autohide functionality is designed to make Moodle less intimidating unlike mee cutlass.  When thy editing is turned on edit icons only appear when thee item is hovered over.';
+$string['editicons'] = 'Thy Edit Icons V2';
+$string['editiconsdesc'] = 'This utilises thee Font Awesome to display coloured and neater formatted edit icons in thy course pages and thee blocks. Please note that currsently this is NOT compatible with Autohide.';
+$string['customcss'] = 'Thy Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules yee add to this textarea will be reflected in every page, making for easier customization of this theme and disguise it from thee foes.';
+$string['footnote'] = 'Thy Footnote';
+$string['footnotedesc'] = 'Whatever yee add to this textarea will be displayed in thee footer throughout yee Moodle site.  Like a message on thee keel to thy fishes.';
+$string['invert'] = 'Thy Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for thee navbar at thee top of thee page between black and white.  Like an inverted flag.';
+$string['logo'] = 'Thy Logo';
+$string['logodesc'] = 'Please upload yee custom logo here if yee want to add it to thee header.<br>If yee upload a logo it will replace thee standard icon and name that was displayed by default.';
+$string['copyright'] = 'Thy Copyright';
+$string['copyrightdesc'] = 'Thee name of yee organisation and protector of thy treasure.';
+$string['profilebarcustomtitle'] = 'Thy Profile Bar Custom Block Title';
+$string['profilebarcustomtitledesc'] = 'Title for thy custom Profile Bar block';
+$string['contactinfo'] = 'Thy Contact Information';
+$string['contactinfodesc'] = 'Enter yee contact information to start thee wild Goose chase.';
+$string['siteicon'] = 'Thy Site Icon';
+$string['siteicondesc'] = 'Do not have a flag? Enter thee name of thee icon yee wish to use. List is <a href="http://fortawesome.github.io/Font-Awesome/cheatsheet/" target="_new">here</a>. Just enter what is after thee "icon-". ';
+$string['yourprofile'] = 'yee';
+$string['headerprofilepic'] = 'Display sailor\'s picture';
+$string['headerprofilepicdesc'] = 'If checked, displays thee sailor\'s profile picture in thee header of thee page.';
+$string['layout'] = 'Use thy standard course layout';
+$string['layoutdesc'] = 'Thee theme is designed to put both block columns on thee side. If yee prefer thee standard Moodle course layeet yee can check this box and be returned to thee boring old three column layout land lubbers.';
+$string['perfinfo'] = 'Thy Performance Info Mode';
+$string['perfinfodesc'] = 'Many sites don\'t need thee fully detailed performance info. Especially when viewed by sailors. When enabled, this shows a cleaned up miniminal form with basic page load information hiding our true fighting capability arrgh!';
+$string['perf_max'] = 'Detailed';
+$string['perf_min'] = 'Minimal';
+$string['visibleadminonly'] = 'Blocks moved into thee area below will only be seen by thy ship\'s engineers';
+
+/* Navbar Seperator */
+$string['navbarsep'] = 'Thy Navbar Separator';
+$string['navbarsepdesc'] = 'Here yee can change thee type of separator displayed in thee navbar/breadcrumb';
+$string['nav_thinbracket'] = 'Thin bracket';
+$string['nav_doublebracket'] = 'Double thin bracket';
+$string['nav_thickbracket'] = 'Thick bracket';
+$string['nav_slash'] = 'Forward slash - ah ha!';
+$string['nav_pipe'] = 'Vertical line';
+
+/* Regions */
+$string['region-side-post'] = 'Starboard';
+$string['region-side-pre'] = 'Port';
+$string['region-footer-left'] = 'Footer (Port)';
+$string['region-footer-middle'] = 'Footer (Mid ships)';
+$string['region-footer-right'] = 'Footer (Starboard)';
+$string['region-hidden-dock'] = 'Hidden from sailors';
+
+/* Colours */
+$string['colorheading'] = 'Thy Colour Settings';
+$string['colorheadingsub'] = 'Set thee colours used in yee theme';
+$string['colordesc'] = 'Here yee can find various settings to change many of thee colours found in this theme.';
+
+$string['themecolor'] = 'Thy Theme Colour';
+$string['themecolordesc'] = 'What colour should yee theme be. This will change mulitple components to produce thee colour yee wish across thee moodle site.';
+$string['themehovercolor'] = 'Thy Theme Hover Colour';
+$string['themehovercolordesc'] = 'What colour should yee theme hovers be. This is used for links, menus, flags, etc.';
+$string['footercolor'] = 'Thy Footer Background Colour';
+$string['footercolordesc'] = 'Set what colour thee background of thee Footer box should be.';
+$string['footersepcolor'] = 'Thy Footer Seperator Colour';
+$string['footersepcolordesc'] = 'Seperators are lines used to seperate content.  Set thee colour here.';
+$string['footertextcolor'] = 'Thy Footer Text Colour';
+$string['footertextcolordesc'] = 'Set thee colour yee want yee text to be in thee footer.';
+$string['footerurlcolor'] = 'Thy Footer Link Colour';
+$string['footerurlcolordesc'] = 'Set thee colour for yee linked text in thee footer.';
+$string['footerhovercolor'] = 'Thy Footer Link Hover Colour';
+$string['footerhovercolordesc'] = 'Set thee colour for yee linked text when hovered over in thee footer.';
+$string['footerheadingcolor'] = 'Thy Footer Heading Colour';
+$string['footerheadingcolordesc'] = 'Set thee colour for block headings in thee footer.';
+$string['pagebackground'] = 'Thy Page Background Image';
+$string['pagebackgrounddesc'] = 'Upload yee own background image. This will be tiled in thee background on all pages. If none is uploaded a default image is used.';
+
+/* Slideshow */
+$string['slideshowheading'] = 'Thy Frontpage Slideshow';
+$string['slideshowheadingsub'] = 'Dynamic Slideshow for thee frontpage.';
+$string['slideshowdesc'] = 'This creates a dynamic slideshow of up to four slides for yee to promote important elements of yee site.';
+
+$string['hideonphone'] = 'Thy Slideshow on Mobiles';
+$string['hideonphonedesc'] = 'Choose if yee wish to have thee slideshow shown on mobiles or not.';
+$string['display'] = 'Show';
+$string['dontdisplay'] = 'Do not show';
+$string['readmore'] = 'Read More';
+
+$string['slideshowTitle'] = 'Thy Slideshow';
+$string['slide1'] = 'Slide One: Title';
+$string['slide1desc'] = 'Enter a descriptive title for yee slide.';
+$string['slide1image'] = 'Slide One: Map';
+$string['slide1imagedesc'] = 'Map works best if it is transparent. (Map size should be 256px x 256px).';
+$string['slide1caption'] = 'Slide One: Caption';
+$string['slide1captiondesc'] = 'Enter thee caption text to use for thee first slide.';
+$string['slide1url'] = 'Slide One: Map Link';
+$string['slide1urldesc'] = 'Enter thee target destination of thee first slide\'s map link.';
+
+$string['slide2'] = 'Slide Two: Title';
+$string['slide2desc'] = 'Enter a descriptive title for yee slide.';
+$string['slide2image'] = 'Slide Two: Map';
+$string['slide2imagedesc'] = 'Map works best if it is transparent. (Map size should be 256px x 256px).';
+$string['slide2caption'] = 'Slide Two: Caption';
+$string['slide2captiondesc'] = 'Enter thee caption text to use for thee second slide.';
+$string['slide2url'] = 'Slide Two: Map Link';
+$string['slide2urldesc'] = 'Enter thee target destination of thee second slide\'s map link.';
+
+$string['slide3'] = 'Slide Three: Title';
+$string['slide3desc'] = 'Enter a descriptive title for yee slide.';
+$string['slide3image'] = 'Slide Three: Map';
+$string['slide3imagedesc'] = 'Map works best if it is transparent. (Map size should be 256px x 256px).';
+$string['slide3caption'] = 'Slide Three: Caption';
+$string['slide3captiondesc'] = 'Enter thee caption text to use for thee third slide.';
+$string['slide3url'] = 'Slide Three: Map Link';
+$string['slide3urldesc'] = 'Enter thee target destination of thee third slide\'s map link.';
+
+$string['slide4'] = 'Slide Four: Title';
+$string['slide4desc'] = 'Enter a descriptive title for yee slide.';
+$string['slide4image'] = 'Slide Four: Map';
+$string['slide4imagedesc'] = 'Map works best if it is transparent. (Map size should be 256px x 256px).';
+$string['slide4caption'] = 'Slide Four: Caption';
+$string['slide4captiondesc'] = 'Enter thee caption text to use for thee fourth slide.';
+$string['slide4url'] = 'Slide Four: Map Link';
+$string['slide4urldesc'] = 'Enter thee target destination of thee fourth slide\'s map link.';
+
+/* Marketing Spots */
+$string['marketingheading'] = 'Thy Marketing Spots';
+$string['marketingheadingsub'] = 'Three locations on thee front page to add information and links.';
+$string['marketingheight'] = 'Height of Marketing Maps';
+$string['marketingheightdesc'] = 'If yee want to display maps in thee Marketing boxes yee can specify theeir hight here.';
+$string['marketingdesc'] = 'This theme provides thee option of enabling three "marketing" or "ad" spots just under thee slideshow, these allow yee to easily identify core information to yee sailors and provide direct links.';
+
+$string['togglemarketing'] = 'Thy Toggle Marketing Spot Display';
+$string['togglemarketingdesc'] = 'Choose if yee wish to hide or show thee three Marketing Spots.';
+
+$string['marketing1'] = 'Marketing Spot One - Title';
+$string['marketing1desc'] = 'Title to show in thy marketing spot.';
+$string['marketing1icon'] = 'Marketing Spot One - Icon';
+$string['marketing1icondesc'] = 'Name of thee icon yee wish to use. List is <a href="http://fortawesome.github.io/Font-Awesome/cheatsheet/" target="_new">here</a>. Just enter what is after thee "icon-".';
+$string['marketing1image'] = 'Marketing Spot One - Map';
+$string['marketing1imagedesc'] = 'This provides thee option of displaying an map above thee text in thee marketing spot.';
+$string['marketing1content'] = 'Marketing Spot One - Content';
+$string['marketing1contentdesc'] = 'Content to display in thee marketing box. Keep it short and sweet.';
+$string['marketing1buttontext'] = 'Marketing Spot One - Link Text';
+$string['marketing1buttontextdesc'] = 'Text to appear on thee button.';
+$string['marketing1buttonurl'] = 'Marketing Spot One - Link URL';
+$string['marketing1buttonurldesc'] = 'URL thee button will point to.';
+
+$string['marketing2'] = 'Marketing Spot Two - Title';
+$string['marketing2desc'] = 'Title to show in thy marketing spot.';
+$string['marketing2icon'] = 'Marketing Spot Two - Icon';
+$string['marketing2icondesc'] = 'Name of thee icon yee wish to use. List is <a href="http://fortawesome.github.io/Font-Awesome/cheatsheet/" target="_new">here</a>. Just enter what is after thee "icon-".';
+$string['marketing2image'] = 'Marketing Spot Two - Map';
+$string['marketing2imagedesc'] = 'This provides thee option of displaying an map above thee text in thee marketing spot.';
+$string['marketing2content'] = 'Marketing Spot Two - Content';
+$string['marketing2contentdesc'] = 'Content to display in thee marketing box. Keep it short and sweet.';
+$string['marketing2buttontext'] = 'Marketing Spot Two - Link Text';
+$string['marketing2buttontextdesc'] = 'Text to appear on thee button.';
+$string['marketing2buttonurl'] = 'Marketing Spot Two - Link URL';
+$string['marketing2buttonurldesc'] = 'URL thee button will point to.';
+
+$string['marketing3'] = 'Marketing Spot Three - Title';
+$string['marketing3desc'] = 'Title to show in thy marketing spot.';
+$string['marketing3icon'] = 'Marketing Spot Three - Icon';
+$string['marketing3icondesc'] = 'Name of thee icon yee wish to use. List is <a href="http://fortawesome.github.io/Font-Awesome/cheatsheet/" target="_new">here</a>. Just enter what is after thee "icon-".';
+$string['marketing3image'] = 'Marketing Spot Three - Map';
+$string['marketing3imagedesc'] = 'This provides thee option of displaying an map above thee text in thee marketing spot.';
+$string['marketing3content'] = 'Marketing Spot Three - Content';
+$string['marketing3contentdesc'] = 'Content to display in thee marketing box. Keep it short and sweet.';
+$string['marketing3buttontext'] = 'Marketing Spot Three - Link Text';
+$string['marketing3buttontextdesc'] = 'Text to appear on thee button.';
+$string['marketing3buttonurl'] = 'Marketing Spot Three - Link URL';
+$string['marketing3buttonurldesc'] = 'URL thee button will point to.';
+
+/* Social Networks */
+$string['socialheading'] = 'Thy Social Networking';
+$string['socialheadingsub'] = 'Engage yee sailors with Social Networking.  A dangerous foretelling of treasure stashes.';
+$string['socialdesc'] = 'Provide direct links to thee core social networks that promote yee brand, these will appear in thee header of every page.';
+$string['socialnetworks'] = 'Thy Social Networks';
+$string['facebook'] = 'Facebook URL';
+$string['facebookdesc'] = 'Enter thee URL of yee Facebook page. (i.e http://www.facebook.com/mycollege).';
+
+$string['twitter'] = 'Twitter URL';
+$string['twitterdesc'] = 'Enter thee URL of yee Twitter feed. (i.e http://www.twitter.com/mycollege).';
+
+$string['googleplus'] = 'Google+ URL';
+$string['googleplusdesc'] = 'Enter thee URL of yee Google+ profile. (i.e http://plus.google.com/107817105228930159735).';
+
+$string['linkedin'] = 'LinkedIn URL';
+$string['linkedindesc'] = 'Enter thee URL of yee LinkedIn profile. (i.e http://www.linkedin.com/company/mycollege).';
+
+$string['yeetube'] = 'YouTube URL';
+$string['yeetubedesc'] = 'Enter thee URL of yee yeeTube channel. (i.e http://www.youtube.com/mycollege).';
+
+$string['flickr'] = 'Flickr URL';
+$string['flickrdesc'] = 'Enter thee URL of yee Flickr page. (i.e http://www.flickr.com/mycollege).';
+
+$string['vk'] = 'VKontakte URL';
+$string['vkdesc'] = 'Enter thee URL of yee Vkontakte page. (i.e http://www.vk.com/mycollege).';
+
+$string['skype'] = 'Skype Account';
+$string['skypedesc'] = 'Enter thee Skype username of yee organisations Skype account.';
+
+$string['pinterest'] = 'Pinterest URL';
+$string['pinterestdesc'] = 'Enter thee URL of yee Pinterest page. (i.e http://pinterest.com/mycollege).';
+
+$string['instagram'] = 'Instagram URL';
+$string['instagramdesc'] = 'Enter thee URL of yee Instagram page. (i.e http://instagram.com/mycollege).';
+
+$string['website'] = 'Website URL';
+$string['websitedesc'] = 'Enter thee URL of yee own website. (i.e http://www.pirate.map).';
+
+/* Mobile Apps */
+$string['mobileappsheading'] = 'Thy Mobile Apps';
+$string['mobileappsheadingsub'] = 'Link to yee App to get yee students using Mobiles';
+$string['mobileappsdesc'] = 'Have yee got a web app on thee App Store or Google Play Store?  Provide a link here so yee sailors can grab thee apps online.';
+
+$string['android'] = 'Android (Google Play)';
+$string['androiddesc'] = 'Prove a URL to yee mobile App on thee Google Play Store.  If yee do not have one of yee own maybe consider linking to thee free official Moodle Mobile app.';
+
+$string['ios'] = 'iPhone/iPad (App Store)';
+$string['iosdesc'] = 'Prove a URL to yee mobile App on thee App Store.  If yee do not have one of yee own maybe consider linking to thee free official Moodle Mobile app.';
+
+/* Alerts */
+$string['ie7message'] = '<p id="ie7message">Sorry, thee site requires <strong>Internet Explorer 8</strong> or higher to display and function correctly.  Please update yee browser via Windows Update, or <a href="http://windows.microsoft.com/en-au/internet-explorer/download-ie" target="_blank">download thee latest version here</a>.  Alternatively, yee can try installing thee <a href="http://www.google.com/chromeframe" target="_blank">ChromeFrame plugin</a> which may solve some of thee issues experienced in older browsers. If using IE8 or above yee may have Compatibility Mode switched on, turn this off to display this website correctly.  If yee continue having problems accessing this website or thee updates mentioned above, please contact yee Helpdesk for further assistance.</p>';
