126f3791374655d89b51d4871c88f4f13f7aa58d
Julian Ridden <julian@moodleman.net>
2013-08-13 22:06:14 +1000

    Updating Moodle minimum requirements
    The method 'blocks()' used by this theme was introduced by Sam in
    MDL-39841 in '/lib/outputrenderers.php' and integrated in core in
    2013051400.05 2.5+ (Build: 20130614).
    
    Older version of Moodle that does not have the method and therefore
    this thee requires 2013051400.05

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2013080900;        // YYYYMMDD.
 $plugin->maturity = MATURITY_STABLE;             // this version's maturity level.
 $plugin->release = '2.5.3 (Build: 20130809)';
-$plugin->requires  = 2013050100;        // Requires Moodle 2.5.
+$plugin->requires  = 2013051400.05;        // Requires Moodle 2.5+.
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array(
     'theme_bootstrapbase'  => 2013050100,
