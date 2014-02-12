fb67f72ff88e3c2631acda2b63daa60c3091ad86
Julian Ridden <julian@moodleman.net>
2014-02-13 08:22:50 +1100

    Add standard_footer_html back to theme

--- layout/includes/footer.php
+++ layout/includes/footer.php
@@ -56,5 +56,6 @@ $hasfooterright = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->re
         echo '<div class="footnote">'.$hasfootnote.'</div>';
     } ?>
 	</div>
+	<?php echo $OUTPUT->standard_footer_html(); ?>
 	
 
