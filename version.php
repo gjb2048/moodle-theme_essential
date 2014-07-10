b43d154385abe72126e84e32158b244db4d58d6c
David Bezemer <david.bezemer@uplearning.nl>
2014-07-10 12:25:24 +0200

    Bump version number for master branch

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014070801;                    // YYYYMMDD.
+$plugin->version   = 2014071000;                    // YYYYMMDDVV.
 $plugin->maturity = MATURITY_STABLE;                // this version's maturity level.
-$plugin->release = '2.7.2 (Build: 20140708)';
+$plugin->release = '2.7.2 (Build: 20140710)';
 $plugin->requires  = 2013101800;                    // Requires Moodle 2.6.
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array(
