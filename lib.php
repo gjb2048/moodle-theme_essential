6442dcf3b3a351318da7112eab33c1bb68366bc3
Gareth J Barnard <gjbarnard@gmail.com>
2014-07-14 18:35:10 +0100

    Alert on frontpage only.

--- lib.php
+++ lib.php
@@ -745,8 +745,8 @@ function theme_essential_showslider($settings) {
 
 function theme_essential_page_init(moodle_page $page) {
     $page->requires->jquery();
-    $page->requires->jquery_plugin('alert', 'theme_essential');
     if ($page->pagelayout == 'frontpage') {
+        $page->requires->jquery_plugin('alert', 'theme_essential');
         $page->requires->jquery_plugin('carousel', 'theme_essential');
     }
     $page->requires->jquery_plugin('collapse', 'theme_essential');
