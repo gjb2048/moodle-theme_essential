3c276b2fb1e6e3c6efde65645b2b34cb3ae2e9bf
David Bezemer <david.bezemer@uplearning.nl>
2014-08-06 16:33:45 +0200

    Use optimized dot png and fully fix #92

--- layout/includes/fonts.php
+++ layout/includes/fonts.php
@@ -49,17 +49,19 @@ case 7:
     $fonts[] = 'Raleway';
 	break;
 }
-?>
-<script type="text/javascript">
-  WebFontConfig = {
-    google: { families: [ <?php foreach ($fonts as $font) { echo "'".$font."'".', '; }; ?> ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })(); </script>
+
+if (!empty($fonts)) {?>
+	<script type="text/javascript">
+	  WebFontConfig = {
+		google: { families: [ <?php foreach ($fonts as $font) { echo "'".$font."'".', '; }; ?> ] }
+	  };
+	  (function() {
+		var wf = document.createElement('script');
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	  })(); </script>
+<?php } ?>
