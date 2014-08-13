5d59b3747f3503798dccd949cd747ecc0c1fa455
Gareth J Barnard <gjbarnard@gmail.com>
2014-08-13 23:42:45 +0100

    Ops!

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,8 +97,6 @@
  * @package theme
  * @subpackage shoehorn
  * @author G J Barnard - gjbarnard at gmail dot com and {@link http://moodle.org/user/profile.php?id=442195}
-
-
  * @author Based on code originally written by Joby Harding, Bas Brands, David Scotson and many other contributors. * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
