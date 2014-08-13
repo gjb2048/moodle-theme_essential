dfa43869dcfeeac209c0df678ed14ff230b73a18
David Bezemer <info@davidbezemer.nl>
2014-08-14 01:50:38 +0200

    Fix #144, thanks Mike Bruns (https://moodle.org/user/view.php?id=1742478)

--- layout/includes/slideshow.php
+++ layout/includes/slideshow.php
@@ -87,8 +87,8 @@ if ($numberofslides) {
                             echo (!empty($PAGE->theme->settings->$urlsetting)? '</a>' : '</div>');
                     } ?>
                 </div>
-                <a class="left carousel-control" href="#essentialCarousel" data-slide="prev"></span><i class="fa fa-chevron-circle-left"></i></a>
-                <a class="right carousel-control" href="#essentialCarousel" data-slide="next"></span><i class="fa fa-chevron-circle-right"></i></a>
+                <a class="left carousel-control" href="#essentialCarousel" data-slide="prev"><i class="fa fa-chevron-circle-left"></i></a>
+                <a class="right carousel-control" href="#essentialCarousel" data-slide="next"><i class="fa fa-chevron-circle-right"></i></a>
             </div>
         </div>
     </div>
