7a1a71bda7ee639ee36fff4559cb824c81665b83
David Bezemer <info@davidbezemer.nl>
2014-08-23 17:07:30 +0200

    Tag new build and simplify version requirement

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014081800;                // YYYYMMDDVV.
+$plugin->version   = 2014082301;                // YYYYMMDDVV.
 $plugin->maturity = MATURITY_BETA;              // this version's maturity level.
-$plugin->release = '2.7.6a (Build: 2014081800)';
-$plugin->requires  = 2014051200.10;                // Requires Moodle 2.7
+$plugin->release = '2.7.6a (Build: 2014082301)';
+$plugin->requires  = 2014050800;                // Requires Moodle 2.7
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array();
