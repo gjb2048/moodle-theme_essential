12908550623cc2df09cdc039987f14fc3db336c2
Gareth J Barnard <gjbarnard@gmail.com>
2014-10-28 17:12:03 +0000

    V2.6.11a Release candidate.

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2014090303; // YYYYMMDDVV.
-$plugin->maturity = MATURITY_STABLE; // this version's maturity level.
+$plugin->maturity = MATURITY_RC; // this version's maturity level.
 $plugin->release = '2.6.11a (Build: 2014090302)';
 $plugin->requires = 2013111800; // Requires Moodle 2.6
 $plugin->component = 'theme_essential';
