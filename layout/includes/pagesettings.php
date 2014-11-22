840a00dab37d136ecc09457d624963ee3aced3f3
Gareth J Barnard <gjbarnard@gmail.com>
2014-11-22 18:07:50 +0000

    Fix footnote regression.

--- layout/includes/pagesettings.php
+++ layout/includes/pagesettings.php
@@ -154,7 +154,7 @@ function essential_has_footer_region() {
 
 /* Group Footer */
 $hascopyright = theme_essential_get_setting('copyright', true);
-$hasfootnote = theme_essential_get_setting('footnote', 'format_text');
+$hasfootnote = theme_essential_get_setting('footnote', 'format_html');
 
 /* Group Breadcrumb */
 $breadcrumbstyle = theme_essential_get_setting('breadcrumbstyle');
