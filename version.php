1fb0a2f0657352a576fbfab5649951228ca4f30b
Gareth J Barnard <gjbarnard@gmail.com>
2015-02-22 16:37:08 +0000

    V2.8.1.1-RC5

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2015022000; // YYYYMMDDVV.
+$plugin->version = 2015022200; // YYYYMMDDVV.
 $plugin->maturity = MATURITY_BETA; // this version's maturity level.
-$plugin->release = '2.8.1.1-RC4 (Build: 2015022000)';
+$plugin->release = '2.8.1.1-RC5 (Build: 2015022200)';
 $plugin->requires  = 2014111000.00; // 2.8 (Build: 20141110).
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array();
