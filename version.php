ce9b8acdedab65c8ae90e1c9041402a7790f6923
Gareth J Barnard <gjbarnard@gmail.com>
2016-02-04 17:34:27 +0000

    Fix #609.

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2015062420; // YYYYMMDDVV.
+$plugin->version = 2015062421; // YYYYMMDDVV.
 $plugin->maturity = MATURITY_STABLE; // This version's maturity level.
-$plugin->release = '2.9.1.7 (Build: 2015062420)';
+$plugin->release = '2.9.1.8 (Build: 2015062421)';
 $plugin->requires  = 2015051100.00; // 2.9 (Build: 20150511).
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array();
