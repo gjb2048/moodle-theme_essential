db9fbe97cc477874be2ed79de69b1360e6b710fa
Gareth J Barnard <gjbarnard@gmail.com>
2016-03-15 17:58:51 +0000

    Colour switcher and slight fix to footer.js for when there is no footer.

--- amd/src/footer.js
+++ amd/src/footer.js
@@ -9,29 +9,31 @@ define(['jquery', 'core/log'], function($, log) {
         init: function() {
             log.debug('Essential Footer AMD init.');
             $(document).ready(function($) {
-                var documentHeight = $(document).height();
-                if ($('html').height() < documentHeight) {
-                    log.debug('Essential Footer AMD adjusting footer position.');
-                    var theOffset = 0;
-                    var footer = $('footer');
-                    var footerOffset = footer.offset().top;
-                    if ($('footer .copy').length) {
-                        var copy = $('footer .copy');
-                        theOffset = (copy.offset().top + copy.outerHeight()) - footerOffset;
-                        log.debug('Calculated footer copyright offset: ' + theOffset + '.');
-                    } else {
-                        var footerperformance = $('footer .footerperformance');
-                        theOffset = footerperformance.offset().top - footerOffset;
-                        log.debug('Calculated footer performance offset: ' + theOffset + '.');
+                if ($('footer').length) { // Might not have a footer.
+                    var documentHeight = $(document).height();
+                    if ($('html').height() < documentHeight) {
+                        log.debug('Essential Footer AMD adjusting footer position.');
+                        var theOffset = 0;
+                        var footer = $('footer');
+                        var footerOffset = footer.offset().top;
+                        if ($('footer .copy').length) {
+                            var copy = $('footer .copy');
+                            theOffset = (copy.offset().top + copy.outerHeight()) - footerOffset;
+                            log.debug('Calculated footer copyright offset: ' + theOffset + '.');
+                        } else {
+                            var footerperformance = $('footer .footerperformance');
+                            theOffset = footerperformance.offset().top - footerOffset;
+                            log.debug('Calculated footer performance offset: ' + theOffset + '.');
+                        }
+                        theOffset = documentHeight - theOffset;
+                        log.debug('Old footer offset: ' + footerOffset + '.');
+                        log.debug('Calculated footer offset: ' + theOffset + '.');
+                        log.debug('Old document height: ' + documentHeight + '.');
+                        footer.offset({ top: theOffset, left: 0});
+                        var newOffset = footer.offset().top;
+                        log.debug('New footer offset: ' + newOffset + '.');
+                        log.debug('New document height: ' + $(document).height() + '.');
                     }
-                    theOffset = documentHeight - theOffset;
-                    log.debug('Old footer offset: ' + footerOffset + '.');
-                    log.debug('Calculated footer offset: ' + theOffset + '.');
-                    log.debug('Old document height: ' + documentHeight + '.');
-                    footer.offset({ top: theOffset, left: 0});
-                    var newOffset = footer.offset().top;
-                    log.debug('New footer offset: ' + newOffset + '.');
-                    log.debug('New document height: ' + $(document).height() + '.');
                 }
             });
         }
