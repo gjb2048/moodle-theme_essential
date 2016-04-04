74a8554563de5e776f6b5818a7879ad08da54509
Gareth J Barnard <gjbarnard@gmail.com>
2016-04-04 16:16:44 +0100

    Version bump.

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2015062424; // YYYYMMDDVV.
+$plugin->version = 2015062425; // YYYYMMDDVV.
 $plugin->maturity = MATURITY_STABLE; // This version's maturity level.
-$plugin->release = '2.9.1.11 (Build: 2015062424)';
+$plugin->release = '2.9.1.12 (Build: 2015062425)';
 $plugin->requires  = 2015051100.00; // 2.9 (Build: 20150511).
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array();
