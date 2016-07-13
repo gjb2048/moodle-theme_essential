2206b38629d88bcd6c9fb0c2a7b8d97509969676
Gareth J Barnard <gjbarnard@gmail.com>
2016-07-13 15:51:12 +0100

    Fix #696.

---
+++ amd/src/navbar.js
@@ -0,0 +1,29 @@
+/**
+ * Essential is a clean and customizable theme.
+ *
+ * @package     theme_essential
+ * @copyright   2016 Gareth J Barnard
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/* jshint ignore:start */
+define(['jquery', 'core/log'], function($, log) {
+
+  "use strict"; // jshint ;_;
+
+  log.debug('Essential navbar AMD');
+
+  return {
+    init: function(data) {
+      $(document).ready(function($) {
+        if (data.oldnavbar) {
+            $('#page-header').css('margin-bottom', $('#essentialnavbar').height() + 'px');
+        } else {
+            $('#page-header').css('margin-top', $('#essentialnavbar').height() + 'px');
+        }
+      });
+      log.debug('Essential navbar AMD init: ' + data);
+    }
+  }
+});
+/* jshint ignore:end */
