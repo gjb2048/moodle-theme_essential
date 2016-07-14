7b65b8f7440ddf56bc9f9f23ca23592a2d5b6cdc
Gareth J Barnard <gjbarnard@gmail.com>
2016-07-14 12:21:09 +0100

    Fix: Two column layout in RTL - M3.1.

--- layout/tiles/twocolumncontent.php
+++ layout/tiles/twocolumncontent.php
@@ -25,7 +25,7 @@
 
 if ($footerregion) {
     echo '<section id="region-main" class="span12">';
-} else if (($hasboringlayout) && ($left)) {
+} else if ((($hasboringlayout) && ($left)) || ((!$hasboringlayout) && (!$left))) {
     echo '<section id="region-main" class="span9 pull-right">';
 } else {
     echo '<section id="region-main" class="span9">';
@@ -38,7 +38,7 @@ if (empty($PAGE->layout_options['nocoursefooter'])) {
 }
 echo '</section>';
 if (!$footerregion) {
-    if (($hasboringlayout) && ($left)) {
+    if ((($hasboringlayout) && ($left)) || ((!$hasboringlayout) && (!$left))) {
         echo $OUTPUT->blocks('side-pre', 'span3 desktop-first-column');
     } else {
         echo $OUTPUT->blocks('side-pre', 'span3');
