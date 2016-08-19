b2d71d7785d21ce5b567a6b46886271affb1e54d
Gareth J Barnard <gjbarnard@gmail.com>
2016-08-19 13:21:47 +0100

    Fix #720.

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -1751,7 +1751,7 @@ class core_renderer extends \core_renderer {
         $regioncontent = '';
         $editing = $this->page->user_is_editing();
         if ($editing) {
-            $regioncontent .= html_writer::tag('span', get_string('region-'.$displayregion, 'theme_essential'),
+            $regioncontent .= html_writer::tag('span', html_writer::tag('span', get_string('region-'.$displayregion, 'theme_essential')),
                 array('class' => 'regionname'));
         }
 
