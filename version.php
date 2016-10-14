016ca09eb51a602e7a2aeb22b9728c8fc07dbe02
Gareth J Barnard <gjbarnard@gmail.com>
2016-10-14 16:12:11 +0100

    Fix: Tidy up Bootstrap override LESS.

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2016010216; // YYYYMMDDVV.
+$plugin->version = 2016010217; // YYYYMMDDVV.
 $plugin->maturity = MATURITY_STABLE; // This version's maturity level.
-$plugin->release = '3.0.1.11 (Build: 2016010216)';
+$plugin->release = '3.0.1.12 (Build: 2016010217)';
 $plugin->requires  = 2015111600.00; // 3.0 (Build: 20151116).
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array();
