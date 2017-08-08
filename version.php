094a7d348c50edc56a710b7e01ddcd7b6801f864
Gareth J Barnard <gjbarnard@gmail.com>
2017-08-08 16:34:54 +0100

    V3.1.1.11

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2016061719; // YYYYMMDDVV.
+$plugin->version = 2016061720; // YYYYMMDDVV.
 $plugin->maturity = MATURITY_STABLE; // This version's maturity level.
-$plugin->release = '3.1.1.10 (Build: 2016061719)';
+$plugin->release = '3.1.1.11 (Build: 2016061720)';
 $plugin->requires  = 2016052300.00; // 3.1 (Build: 20160523).
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array(
