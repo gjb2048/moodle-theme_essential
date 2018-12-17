a74cb752df7137228f203225ef55422f3172701a
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2018-12-17 13:04:50 +0000

    Fix scroll linked effects on FireFox.

--- amd/src/affix.js
+++ amd/src/affix.js
@@ -18,38 +18,7 @@ define(['jquery', 'theme_bootstrapbase/bootstrap', 'core/log'], function($, boot
         init: function() {
             $(document).ready(function($) {
                 if ($("#essentialnavbar").length) {
-                    var $essentialnavbar = $("#essentialnavbar");
-                    var pageheaderHeight = $("#page-header").height();
-                    log.debug('Essential affix AMD pageheaderHeight: ' + pageheaderHeight);
-                    $essentialnavbar.affix({
-                        offset: {
-                            top: function() {
-                                pageheaderHeight = $("#page-header").height();
-                                log.debug('Essential affix offset AMD phh: ' + pageheaderHeight);
-                                var wst = $(window).scrollTop();
-                                log.debug('Essential affix offset AMD wst: ' + wst);
-                                var diff = pageheaderHeight - wst;
-                                log.debug('Essential affix offset AMD diff: ' + diff);
-                                if (diff < 0) {
-                                    diff = 0;
-                                }
-                                $essentialnavbar.css('top', diff + 'px');
-                                return pageheaderHeight;
-                            }
-                        }
-                    });
-                    $(window).resize(function() {
-                        pageheaderHeight = $("#page-header").height();
-                        log.debug('Essential affix resize AMD phh: ' + pageheaderHeight);
-                        var wst = $(window).scrollTop();
-                        log.debug('Essential affix resize AMD wst: ' + wst);
-                        var diff = pageheaderHeight - wst;
-                        log.debug('Essential affix resize AMD diff: ' + diff);
-                        if (diff < 0) {
-                            diff = 0;
-                        }
-                        $essentialnavbar.css('top', diff + 'px');
-                    });
+                    $("#essentialnavbar").affix();
                 }
             });
             log.debug('Essential affix AMD init');
