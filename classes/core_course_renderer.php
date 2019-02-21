998bb9df2bd8e8343cc7049534cece1720b3c6e8
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2019-02-21 17:23:38 +0000

    Fix: Coursecat depreciation message when 'enablecategoryicon' is true.

--- classes/core_course_renderer.php
+++ classes/core_course_renderer.php
@@ -258,8 +258,7 @@ class theme_essential_core_course_renderer extends core_course_renderer {
 
         // Display course category if necessary (for example in search results).
         if ($chelper->get_show_courses() == self::COURSECAT_SHOW_COURSES_EXPANDED_WITH_CAT) {
-            require_once($CFG->libdir. '/coursecatlib.php');
-            if ($cat = coursecat::get($course->category, IGNORE_MISSING)) {
+            if ($cat = core_course_category::get($course->category, IGNORE_MISSING)) {
                 $content .= html_writer::start_tag('div', array('class' => 'coursecat'));
                 $content .= get_string('category').': '.
                         html_writer::link(new moodle_url('/course/index.php', array('categoryid' => $cat->id)),
